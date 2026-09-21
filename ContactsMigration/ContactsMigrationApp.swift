2ac1dd3aacaee24ab748cf7ab14e97948446cc6d
Lisa Fellows <100980689+lisa-appfellows@users.noreply.github.com>
2026-09-21 13:56:55 -0600

    chore: project setup with ci pipeline

---
+++ ContactsMigration/ContactsMigrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactsMigrationApp.swift
+//  ContactsMigration
+//
+//  Created by Lisa Fellows on 2026-09-21.
+//
+
+import SwiftUI
+
+@main
+struct ContactsMigrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
